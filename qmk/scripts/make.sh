2bd803836c206521303f4146dd5cd123d1c66c1b
Marcelo Lima <m-lima@users.noreply.github.com>
2024-09-16 20:14:45 +0200

    Update build script

--- qmk/scripts/make.sh
+++ qmk/scripts/make.sh
@@ -4,6 +4,32 @@
 # The keymap should be available under `qmk_firmware/keyboards/cantor/keymaps/nantor`
 
 if [ ! ${1} ]; then
-  echo '[33mNo version provided[m'
+  echo '[31mNo version provided[m'
+  exit 1
 fi
-docker run -it --rm --volume ${PWD}:/src --name qmk qmkfm/qmk_cli bash -c "cd src/; make cantor:nantor${1}"
+
+if [[ "${1}" = v* ]]; then
+  version="${1}"
+else
+  version="v${1}"
+fi
+
+root=$(realpath "${0}")
+root=$(dirname "${root}")
+
+layout="${root}/../layout/${version}"
+if [ ! -d "${layout}" ]; then
+  echo '[31mThe version `'"${version}"'` does not exist in `'"${layout}"'`[m'
+  exit 1
+fi
+layout=$(realpath "${layout}")
+
+root="${root}/../qmk_firmware"
+root=$(realpath "${root}")
+
+if grep SERIAL_USART_FULL_DUPLEX "${root}/keyboards/cantor/config.h" &> /dev/null; then
+  sed -i '' 's/.*SERIAL_USART_FULL_DUPLEX.*//g' "${root}/keyboards/cantor/config.h"
+fi
+
+echo 'Docker image id: 9ced503a5714'
+docker run -it --rm --volume "${root}:/src" --volume "${layout}:/src/keyboards/cantor/keymaps/nantor" --name qmk qmkfm/qmk_cli bash -c "cd src/; make cantor:nantor"
