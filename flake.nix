71a16d8f073d861ee605c503b840326e650fc1bb
Marcelo Lima <m-lima@users.noreply.github.com>
2025-08-30 01:32:18 +0200

    Start nixifying

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          kicad = pkgs.mkShell {
+            buildInputs = [
+              pkgs.kicad
+            ];
+          };
+          inkscape = pkgs.mkShell {
+            buildInputs = [
+              pkgs.inkscape
+            ];
+          };
+        };
+      }
+    );
+}
