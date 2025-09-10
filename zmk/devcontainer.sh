72c702784c4cfebf8aa2eb617f026bd10c1a4791
Marcelo Lima <m-lima@users.noreply.github.com>
2025-09-10 14:27:36 +0200

    Update zmk

--- zmk/devcontainer.sh
+++ zmk/devcontainer.sh
@@ -27,7 +27,7 @@ function check_volume {
 }
 
 function check_submodule {
-  if ! (git submodule status | grep heads/main &> /dev/null); then
+  if (git submodule status | grep zmk | grep '^-' &> /dev/null); then
     git submodule update --init --recursive -- "${root}/zmk"
   fi
 }
